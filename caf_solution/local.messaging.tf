e9084ae53f73eea0a51bb986f4a68d376fc555bb
Darren Hull <darren.hull@foundationhomeloans.co.uk>
2024-06-14 16:26:14 +0000

    added eventgrid namespace topic

--- caf_solution/local.messaging.tf
+++ caf_solution/local.messaging.tf
@@ -2,18 +2,19 @@ locals {
   messaging = merge(
     var.messaging,
     {
-      eventgrid_domain             = var.eventgrid_domain
-      eventgrid_domain_topic       = var.eventgrid_domain_topic
-      eventgrid_event_subscription = var.eventgrid_event_subscription
-      eventgrid_topic              = var.eventgrid_topic
-      eventgrid_namespaces         = var.eventgrid_namespaces
-      eventgrid_namespace_topics   = var.eventgrid_namespace_topics
-      servicebus_namespaces        = var.servicebus_namespaces
-      servicebus_queues            = var.servicebus_queues
-      servicebus_topics            = var.servicebus_topics
-      signalr_services             = var.signalr_services
-      web_pubsub_hubs              = var.web_pubsub_hubs
-      web_pubsubs                  = var.web_pubsubs
+      eventgrid_domain                          = var.eventgrid_domain
+      eventgrid_domain_topic                    = var.eventgrid_domain_topic
+      eventgrid_event_subscription              = var.eventgrid_event_subscription
+      eventgrid_topic                           = var.eventgrid_topic
+      eventgrid_namespaces                      = var.eventgrid_namespaces
+      eventgrid_namespace_topics                = var.eventgrid_namespace_topics
+      eventgrid_namespace_topic_subscriptions   = var.eventgrid_namespace_topic_subscriptions
+      servicebus_namespaces                     = var.servicebus_namespaces
+      servicebus_queues                         = var.servicebus_queues
+      servicebus_topics                         = var.servicebus_topics
+      signalr_services                          = var.signalr_services
+      web_pubsub_hubs                           = var.web_pubsub_hubs
+      web_pubsubs                               = var.web_pubsubs
     }
   )
 }
